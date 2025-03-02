4d03902b89d8aed2c102823fcf3f6884c834baa3
LWyattBarnett <wbarnett1992@gmail.com>
2025-03-01 20:33:16 -0700

    Create skis.js

---
+++ data/skis.js
@@ -0,0 +1,5 @@
+[
+    {"brand": "BrandA", "model": "Model1", "image": "images/ski1.jpg"},
+    {"brand": "BrandA", "model": "Model2", "image": "images/ski2.jpg"},
+    {"brand": "BrandB", "model": "Model3", "image": "images/ski3.jpg"}
+]
